32533e995e97950d2c430785cba1b059b9600159
liza-kl <hello@li54.dev>
2022-01-12 21:49:13 +0100

    :recycle: some stuff for App

--- crr-frontend/src/App.js
+++ crr-frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 import RegisterClimbingRouteComponent from "./components/RegisterClimbingRoute/RegisterClimbingRoute.component";
-import {Grid} from "@mui/material";
+import {Grid, Stack} from "@mui/material";
 
 function App() {
   return (
-    <Grid container spacing={2}>
-        <Grid item xs={12}>
+    <Grid container spacing={2}
+          className="App"
+          justifyContent="center"
+          alignItems="center">
+        <Grid item xs={12} md={4}>
            <RegisterClimbingRouteComponent/>
         </Grid>
     </Grid>
